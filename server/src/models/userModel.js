c7a2843ecf0048cf6ace70c69e0126c8f488d055
Mukul Rana <mukulgenious123@gmail.com>
2024-10-08 17:42:23 +0530

    updated user schema

--- server/src/models/userModel.js
+++ server/src/models/userModel.js
@@ -1,4 +1,4 @@
-import { Schema } from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
 const userSchema = new Schema({
   name: {
@@ -6,32 +6,70 @@ const userSchema = new Schema({
     required: true,
     trim: true,  
   },
+  profileName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  gender: {
+    type: String,
+    enum: ['male', 'female', 'other'], // Gender selection
+    required: true
+  },
   dob: {
-    type: Date, 
+    type: Date,
+  },
+  phone: {
+    type: Number,
+    required: true,
+    maxlength: 10  // Maximum length for US phone numbers
   },
   email: {
     type: String,
     required: true,
     unique: true, 
-    lowercase: true, 
-    validate: {
-      validator: function (v) {
-        return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(v);
-      },
-      message: props => `${props.value} is not a valid email address!`,
-    }
+    lowercase: true,
+    trim: true,
   },
   password: {
     type: String,
     required: true,
-    minlength: 6,  
+    minlength: 6,
   },
-  profile: {
+  profile: {  // Profile picture URL
     type: String,
     default: "",  
+  },
+  attributes: {
+    type: [{
+      currentSelf: {
+        type: [String], // Array of user behaviors for current self
+        default: ["Unrelaxed", "Absent minded", "Afraid", "Exhausted"]
+      },
+      imagineSelf: {
+        type: [String], // Array of user behaviors for imagined self
+        default: ["Intelligent", "Wealthy", "Patient", "Social"]
+      }
+    }],
+    default: [{}],  // Ensure default structure for both `currentSelf` and `imagineSelf`
+  },
+  paymentMethods: {
+    type: [{
+      methodType: {
+        type: String,
+        enum: ['Credit Card', 'Debit Card', 'PayPal', 'Bank Transfer'],
+      },
+      cardNumber: {
+        type: String,
+      },
+      expiryDate: {
+        type: String,  
+      }
+    }],
+    default: []  // No payment methods by default
   }
 }, {
-  timestamps: true, 
+  timestamps: true, // Automatically adds createdAt and updatedAt timestamps
 });
 
 export default mongoose.model('User', userSchema);
